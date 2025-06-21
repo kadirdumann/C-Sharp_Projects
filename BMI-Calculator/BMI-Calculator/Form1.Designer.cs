16319f0d167fe379e652779b84352a037d3f3506
kadir <kadirdmn38@gmail.com>
2025-06-21 23:40:45 +0300

    BMI Project Added

---
+++ BMI-Calculator/BMI-Calculator/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.surnameLbl = new System.Windows.Forms.Label();
+            this.lengthLbl = new System.Windows.Forms.Label();
+            this.weightLbl = new System.Windows.Forms.Label();
+            this.genderLbl = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.surnameBox = new System.Windows.Forms.TextBox();
+            this.lengthBox = new System.Windows.Forms.TextBox();
+            this.weightBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLbl.Location = new System.Drawing.Point(57, 108);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(43, 28);
+            this.nameLbl.TabIndex = 0;
+            this.nameLbl.Text = "Ad";
+            // 
+            // surnameLbl
+            // 
+            this.surnameLbl.AutoSize = true;
+            this.surnameLbl.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.surnameLbl.Location = new System.Drawing.Point(57, 153);
+            this.surnameLbl.Name = "surnameLbl";
+            this.surnameLbl.Size = new System.Drawing.Size(69, 28);
+            this.surnameLbl.TabIndex = 1;
+            this.surnameLbl.Text = "Soyad";
+            // 
+            // lengthLbl
+            // 
+            this.lengthLbl.AutoSize = true;
+            this.lengthLbl.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lengthLbl.Location = new System.Drawing.Point(57, 204);
+            this.lengthLbl.Name = "lengthLbl";
+            this.lengthLbl.Size = new System.Drawing.Size(98, 28);
+            this.lengthLbl.TabIndex = 2;
+            this.lengthLbl.Text = "Boy  (cm)";
+            // 
+            // weightLbl
+            // 
+            this.weightLbl.AutoSize = true;
+            this.weightLbl.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightLbl.Location = new System.Drawing.Point(57, 248);
+            this.weightLbl.Name = "weightLbl";
+            this.weightLbl.Size = new System.Drawing.Size(96, 28);
+            this.weightLbl.TabIndex = 3;
+            this.weightLbl.Text = "Kilo  (kg)";
+            // 
+            // genderLbl
+            // 
+            this.genderLbl.AutoSize = true;
+            this.genderLbl.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genderLbl.Location = new System.Drawing.Point(57, 294);
+            this.genderLbl.Name = "genderLbl";
+            this.genderLbl.Size = new System.Drawing.Size(91, 28);
+            this.genderLbl.TabIndex = 4;
+            this.genderLbl.Text = "Cinsiyet";
+            // 
+            // comboBox
+            // 
+            this.comboBox.BackColor = System.Drawing.SystemColors.Info;
+            this.comboBox.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Bay",
+            "Bayan"});
+            this.comboBox.Location = new System.Drawing.Point(170, 298);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(148, 30);
+            this.comboBox.TabIndex = 7;
+            // 
+            // nameBox
+            // 
+            this.nameBox.BackColor = System.Drawing.SystemColors.Info;
+            this.nameBox.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nameBox.Location = new System.Drawing.Point(170, 108);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(130, 27);
+            this.nameBox.TabIndex = 8;
+            // 
+            // surnameBox
+            // 
+            this.surnameBox.BackColor = System.Drawing.SystemColors.Info;
+            this.surnameBox.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.surnameBox.Location = new System.Drawing.Point(170, 153);
+            this.surnameBox.Name = "surnameBox";
+            this.surnameBox.Size = new System.Drawing.Size(130, 27);
+            this.surnameBox.TabIndex = 9;
+            // 
+            // lengthBox
+            // 
+            this.lengthBox.BackColor = System.Drawing.SystemColors.Info;
+            this.lengthBox.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lengthBox.Location = new System.Drawing.Point(170, 204);
+            this.lengthBox.Name = "lengthBox";
+            this.lengthBox.Size = new System.Drawing.Size(130, 27);
+            this.lengthBox.TabIndex = 10;
+            // 
+            // weightBox
+            // 
+            this.weightBox.BackColor = System.Drawing.SystemColors.Info;
+            this.weightBox.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.weightBox.Location = new System.Drawing.Point(170, 248);
+            this.weightBox.Name = "weightBox";
+            this.weightBox.Size = new System.Drawing.Size(130, 27);
+            this.weightBox.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Ink Free", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(217, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 53);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Vücut Kitle İndeksi";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(98, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 65);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(352, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(356, 213);
+            this.listBox1.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "vke";
+            this.label2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(529, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 65);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.weightBox);
+            this.Controls.Add(this.lengthBox);
+            this.Controls.Add(this.surnameBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.genderLbl);
+            this.Controls.Add(this.weightLbl);
+            this.Controls.Add(this.lengthLbl);
+            this.Controls.Add(this.surnameLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Name = "form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label surnameLbl;
+        private System.Windows.Forms.Label lengthLbl;
+        private System.Windows.Forms.Label weightLbl;
+        private System.Windows.Forms.Label genderLbl;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox surnameBox;
+        private System.Windows.Forms.TextBox lengthBox;
+        private System.Windows.Forms.TextBox weightBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BMI-Calculator/BMI-Calculator/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = nameBox.Text;
+            string soyad = surnameBox.Text;
+            double boy = Convert.ToDouble(lengthBox.Text); // textbox'tan boy değerini al
+            double kilo = Convert.ToDouble(weightBox.Text); // textbox'tan kilo değerini al
+            string cinsiyet = comboBox.Items.Count.ToString();
+
+            //double vke = kilo / (boy * boy); // vücut kitle indeksi hesabı
+            double vke = kilo / ((boy / 100.0) * (boy / 100.0));
+
+            // Vücut kitle endeksi yorumu
+            string yorum = "";
+            if (cinsiyet == "Bay")
+            {
+                if (vke < 20.7)
+                {
+                    yorum = "Zayıf";
+                }
+                else if (vke >= 20.7 && vke < 26.4)
+                {
+                    yorum = "Normal";
+                }
+                else
+                {
+                    yorum = "Fazla Kilolu";
+                }
+
+            }
+            else
+            {
+                if (vke < 19.1)
+                {
+                    yorum = "Zayıf";
+                }
+                else if (vke >= 19.1 && vke < 25.8)
+                {
+                    yorum = "Normal";
+                }
+                else
+                {
+                    yorum = "Fazla Kilolu";
+                }
+            }
+            label2.Text = yorum;
+
+            listBox1.Items.Add(ad + " " + soyad + " - " + vke.ToString("F") + " - " + label2.Text);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
